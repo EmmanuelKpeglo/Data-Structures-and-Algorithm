e3ea1eab24c52f441299cd4f5c8107174050fb6d
EmmanuelKpeglo <emmanuel.kpeglo@turntabl.io>
2023-05-30 12:48:36 +0000

    ...

--- com/company/Main.java
+++ com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("ytu");
+	    // write your code here
 
     }
 }
