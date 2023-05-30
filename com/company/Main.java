eeabb4a82f741eb3fde2dcd3014db146bf8c077d
EmmanuelKpeglo <emmanuel.kpeglo@turntabl.io>
2023-05-30 20:01:33 +0000

    reverse string

--- com/company/Main.java
+++ com/company/Main.java
@@ -5,22 +5,15 @@ import com.company.LinkedList.LinkedList;
 import com.company.Stacks.Stacks;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        var stack = new Stacks();
-        stack.push(4);
-        stack.push(8);
-        stack.push(7);
-        System.out.println(stack.pop());
-        System.out.println(stack.peek());
-        System.out.println(stack.empty());
-        var eight = stack.pop();
-        var four = stack.pop();
-        System.out.println(eight);
-        System.out.println(four);
-        System.out.println(stack.empty());
+
+        var input = "hello world";
+        var reverser = new StringReverser();
+        System.out.println(reverser.reverse(input));
     }
 }
---
+++ com/company/StringReverser.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Stack;
+
+public class StringReverser {
+    public String reverse(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+        var splittedString = input.split("");
+        Stack<String> stack = new Stack<>();
+        for (int count = 0; count < splittedString.length; count++) {
+            stack.push(splittedString[count]);
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!stack.empty()) {
+            stringBuilder.append(stack.pop());
+        }
+
+        return stringBuilder.toString();
+    }
+}
